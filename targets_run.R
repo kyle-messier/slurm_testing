716cbeeeb0a6d61aac66f3fecb2570595c7c5d04
kyle-messier <kyle.messier@nih.gov>
2025-09-22 23:28:50 -0400

    renv

--- targets_run.R
+++ targets_run.R
@@ -1,14 +1,14 @@
-# # Check .libPaths().
-# cat("Active library paths:\n")
-# .libPaths()
+# Check .libPaths().
+cat("Active library paths:\n")
+.libPaths()
 
-# # Check PATH.
-# cat("Active PATH:\n")
-# Sys.getenv("PATH")
+# Check PATH.
+cat("Active PATH:\n")
+Sys.getenv("PATH")
 
-# # Check LD_LIBRARY_PATH
-# cat("Active LD_LIBRARY_PATH:\n")
-# Sys.getenv("LD_LIBRARY_PATH")
+# Check LD_LIBRARY_PATH
+cat("Active LD_LIBRARY_PATH:\n")
+Sys.getenv("LD_LIBRARY_PATH")
 
 ############################      RUN PIPELINE      ############################
 targets::tar_make(
